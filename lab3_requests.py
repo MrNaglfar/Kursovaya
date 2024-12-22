385fcba9afefebc3c90756acdbe07ec866dcd480
MrNaglfar <160016349+MrNaglfar@users.noreply.github.com>
2024-12-23 01:29:44 +0300

    Update lab3_requests.py

--- lab3_requests.py
+++ lab3_requests.py
@@ -1,23 +1,31 @@
-from models import *
+from models import User, Book, Author, Genre, BookIssue
 from sqlmodel import Session, select
 
-def get_reader_by_id(reader_id: int):
+def get_user_by_id(user_id: int):
     with Session(engine) as session:
-        statement = select(Reader).where(Reader.readerID == reader_id)
+        statement = select(User).where(User.user_id == user_id)
         return session.exec(statement).first()
-
 
 def get_all_books():
     with Session(engine) as session:
         statement = select(Book)
         return session.exec(statement).all()
 
+def get_books_by_author(author_id: int):
+    with Session(engine) as session:
+        statement = select(Book).where(Book.author_id == author_id)
+        return session.exec(statement).all()
+
+
 if __name__ == "__main__":
-    reader = get_reader_by_id(1)
-    if reader:
-        print(f"Читатель: {reader}")
+    user = get_user_by_id(1)
+    if user:
+        print(f"Пользователь: {user}")
     else:
-        print("Читатель не найден")
+        print("Пользователь не найден")
 
     books = get_all_books()
     print(f"Все книги: {books}")
+
+    books_by_author = get_books_by_author(1) 
+    print(f"Книги автора с id=1: {books_by_author}")
